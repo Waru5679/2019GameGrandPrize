7169259ca1044fd06b4f8f8b2f940cd7a45e54c8
友樹 勝目 <h1201616007@hiratagakuen.onmicrosoft.com>
2019-05-10 06:41:58 +0900

    タイトルからステージ選択へ移行追加、マップ選択によってマップ変更追加、敵が2種とも弾に当たったら死ぬのを追加

--- Game/Map.cpp
+++ Game/Map.cpp
@@ -25,6 +25,12 @@ void CMap::Init()
 	//スクロール移動
 	m_fScroll = SCROLL_SPEED;
 
+	//生成フラグをオンにする
+	m_bFirstTimeSide = true;
+	m_bFirstTimeVertical = true;
+	//スクロールチェンジをオフにする
+	m_bScrollChangeIs = false;
+
 }
 
 //更新
@@ -52,14 +58,34 @@ void CMap::Action()
 	//縦
 	if (m_bScroll == SIDE)
 	{
-		//オブジェクト生成(横)
-		CreateSide();
+		//if (m_bFirstTimeSide == true)
+		//{
+		//	//オブジェクト生成(横)
+		//	CreateSideFirst();
+		//}
+		//else
+		//{
+		//	CreateSideSecond();
+		//}
+
+		CreateSideFirst();
+
 	}
 	else
 	{
-		//オブジェクト生成(縦)
-		CreateVertical();
+		//if (m_bFirstTimeVertical == true)
+		//{
+		//	//オブジェクト生成(縦)
+		//	CreateVerticalFirst();
+		//}
+		//else
+		//{
+		//	CreateVerticalSecond();
+		//}
+
+		CreateVerticalFirst();
 	}
+
 }
 
 //描画
@@ -68,8 +94,8 @@ void CMap::Draw()
 	
 }
 
-//生成(横)
-void CMap::CreateSide()
+//生成初回(横)
+void CMap::CreateSideFirst()
 {
 	for (int x = 0; x < SIDE_MAX_X; x++)
 	{
@@ -96,10 +122,10 @@ void CMap::CreateSide()
 					//生成が終われば空白に
 					m_SideMap[y][x] = MAP_NONE;
 					break;
-				}
-				//敵
+ 				}
+						//敵
 				case MAP_ENEMY:
-				{
+ 				{
 					CEnemy* pEnemy = new CEnemy(x, y);
 					Objs::InsertObj(pEnemy, OBJ_ENEMY, 10);
 
@@ -153,12 +179,17 @@ void CMap::CreateSide()
 				//スクロールチェンジ
 				case MAP_SCROLL_CHANGE:
 				{
-					CScrollChange* pScrollChange = new  CScrollChange(x, y,SIDE);
+					CScrollChange* pScrollChange = new  CScrollChange(x, y, SIDE);
 					Objs::InsertObj(pScrollChange, OBJ_SCROLL_CHANGE, 10);
 
 					//生成が終われば空白に
 					m_SideMap[y][x] = MAP_NONE;
+
+					//生成場所を記憶
+					/*((UserData*)Save::GetData())->m_iMap_x = x;
+					m_bScrollChangeIs = true;*/
 					break;
+						
 				}
 				//ゴール
 				case MAP_GOAL:
@@ -174,12 +205,145 @@ void CMap::CreateSide()
 			}
 
 		}
+		//if (m_bScrollChangeIs == true)
+		//{
+		//	//初回生成をオフにする
+		//	m_bFirstTimeSide = false;
+		//	break;
+		//}
+		//
+	}
+	
+}
+
+void CMap::CreateSideSecond()
+{
+	m_bScrollChangeIs = false;
+
+	//for (int x = ((UserData*)Save::GetData)->m_iMap_x; x < SIDE_MAX_X; x++)
+	for (int x = 0; x < SIDE_MAX_X; x++)
+	{
+		for (int y = 0; y < SIDE_MAX_Y; y++)
+		{
+			switch (m_SideMap[y][x])
+			{
+				//地面
+			case MAP_PLANE:
+			{
+				CPlane* pPlane = new CPlane(x, y);
+				Objs::InsertObj(pPlane, OBJ_PLANE, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+				break;
+			}
+			//ブラックホール
+			case MAP_BLACK_HOLE:
+			{
+				CBlackHole* pBlackHole = new CBlackHole(x, y);
+				Objs::InsertObj(pBlackHole, OBJ_BLACK_HOLE, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+				break;
+			}
+			//敵
+			case MAP_ENEMY:
+			{
+				CEnemy* pEnemy = new CEnemy(x, y);
+				Objs::InsertObj(pEnemy, OBJ_ENEMY, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+				break;
+			}
+			//星
+			case MAP_STAR:
+			{
+				CStar* pStar = new CStar(x, y);
+				Objs::InsertObj(pStar, OBJ_STAR, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+
+				break;
+			}
+			//アイテム(大)
+			case MAP_ITEM_BIG:
+			{
+				CCollectionItemBig* pCollection_big = new  CCollectionItemBig(x, y, m_iMapLoop);
+				Objs::InsertObj(pCollection_big, OBJ_ITEM_BIG, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+
+				//マップ内のアイテムの種類識別
+				m_iMapItem[m_iMapLoop] = ITEM_BIG;
+
+				m_iMapLoop++;
+
+				break;
+			}
+			//アイテム(小)
+			case MAP_ITEM_SMALL:
+			{
+				CCollectionItemSmall* pCollection_small = new  CCollectionItemSmall(x, y, m_iMapLoop);
+				Objs::InsertObj(pCollection_small, OBJ_ITEM_SMALL, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+
+				//マップ内のアイテムの種類識別
+				m_iMapItem[m_iMapLoop] = ITEM_SMALL;
+
+				m_iMapLoop++;
+
+				break;
+			}
+			//スクロールチェンジ
+			case MAP_SCROLL_CHANGE:
+			{
+				CScrollChange* pScrollChange = new  CScrollChange(x, y, SIDE);
+				Objs::InsertObj(pScrollChange, OBJ_SCROLL_CHANGE, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+
+				//生成場所を記憶
+				/*((UserData*)Save::GetData())->m_iMap_x = x;
+				m_bScrollChangeIs = true;*/
+				break;
+
+			}
+			//ゴール
+			case MAP_GOAL:
+			{
+				CGoal* pClear = new CGoal(x, y);
+				Objs::InsertObj(pClear, OBJ_GAME_CLEAR, 10);
+
+				//生成が終われば空白に
+				m_SideMap[y][x] = MAP_NONE;
+				break;
+			}
+
+			}
+
+		}
+		//if (m_bScrollChangeIs == true)
+		//{
+		//	//初回生成をオフにする
+		//	m_bFirstTimeSide = false;
+		//	break;
+		//}
+
 	}
 }
 
-//生成(縦)
-void CMap::CreateVertical()
+//生成初回(縦)
+void CMap::CreateVerticalFirst()
 {
+	m_bScrollChangeIs = false;
+
 	for (int y = VARTICAL_MAX_Y - 1; y > 0; y--)
 	{
 		for (int x = 0; x < VARTICAL_MAX_X; x++)
@@ -257,6 +421,9 @@ void CMap::CreateVertical()
 
 					//生成が終われば空白に
 					m_VarticalMap[y][x] = MAP_NONE;
+
+					/*((UserData*)Save::GetData())->m_iMap_y = y;
+					m_bScrollChangeIs = true;*/
 					break;
 				}
 				//上下移動敵
@@ -272,5 +439,118 @@ void CMap::CreateVertical()
 			}
 
 		}
+
+		if (m_bScrollChangeIs == true)
+		{
+			//初回生成をオフにする
+			/*m_bFirstTimeSide = false;
+			break;*/
+		}
+	}
+}
+
+void CMap::CreateVerticalSecond()
+{
+	for (int y = VARTICAL_MAX_Y - 1; y > 0; y--)
+	{
+		for (int x = 0; x < VARTICAL_MAX_X; x++)
+		{
+			switch (m_VarticalMap[y][x])
+			{
+				//ブラックホール
+			case MAP_BLACK_HOLE:
+			{
+				CBlackHole* pBlackHole = new CBlackHole(x, y - VARTICAL_MAX_Y);
+				Objs::InsertObj(pBlackHole, OBJ_BLACK_HOLE, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+				break;
+			}
+			//敵
+			case MAP_ENEMY:
+			{
+				CEnemy* pEnemy = new CEnemy(x, y - VARTICAL_MAX_Y);
+				Objs::InsertObj(pEnemy, OBJ_ENEMY, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+				break;
+			}
+			//星
+			case MAP_STAR:
+			{
+				CStar* pStar = new CStar(x, y - VARTICAL_MAX_Y);
+				Objs::InsertObj(pStar, OBJ_STAR, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+
+				break;
+			}
+			//アイテム(大)
+			case MAP_ITEM_BIG:
+			{
+				CCollectionItemBig* pCollection_big = new  CCollectionItemBig(x, y - VARTICAL_MAX_Y, m_iMapLoop);
+				Objs::InsertObj(pCollection_big, OBJ_ITEM_BIG, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+
+				//マップ内のアイテムの種類識別
+				m_iMapItem[m_iMapLoop] = ITEM_BIG;
+
+				m_iMapLoop++;
+
+				break;
+			}
+			//アイテム(小)
+			case MAP_ITEM_SMALL:
+			{
+				CCollectionItemSmall* pCollection_small = new  CCollectionItemSmall(x, y - VARTICAL_MAX_Y, m_iMapLoop);
+				Objs::InsertObj(pCollection_small, OBJ_ITEM_SMALL, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+
+				//マップ内のアイテムの種類識別
+				m_iMapItem[m_iMapLoop] = ITEM_SMALL;
+
+				m_iMapLoop++;
+
+				break;
+			}
+			//スクロールチェンジ
+			case MAP_SCROLL_CHANGE:
+			{
+				CScrollChange* pScrollChange = new  CScrollChange(x, y - VARTICAL_MAX_Y, VARTICAL);
+				Objs::InsertObj(pScrollChange, OBJ_SCROLL_CHANGE, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+
+				/*((UserData*)Save::GetData())->m_iMap_y = y;
+				m_bScrollChangeIs = true;*/
+				break;
+			}
+			//上下移動敵
+			case MAP_ENEMY_UPDOWN:
+			{
+				CEnemyUpDown* pEnemyUpDown = new CEnemyUpDown(x, y - VARTICAL_MAX_Y);
+				Objs::InsertObj(pEnemyUpDown, OBJ_ENEMY_UPDOWN, 10);
+
+				//生成が終われば空白に
+				m_VarticalMap[y][x] = MAP_NONE;
+				break;
+			}
+			}
+
+		}
+		//if (m_bScrollChangeIs == true)
+		//{
+		//	//初回生成をオフにする
+		//	m_bFirstTimeSide = false;
+		//	break;
+		//}
 	}
 }
